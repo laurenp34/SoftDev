1cb6824e9b58a39c91b1f87229fe56d6fe258feb
Lauren <laurenpehlivanian@gmail.com>
2019-09-15 19:26:19 +0100

    all methods work now

--- randomOccupation.py
+++ randomOccupation.py
@@ -10,9 +10,8 @@ def genDict(filename):
         line_count = 0                                      #define the line count
         for row in reader:                                  #for each row do this
             if line_count > 0 and row[0] != 'Total':        #if the line is not the first or last line do this
-                occupdict[row[0]] = float(row[1])                  #add the occupation as the key and the percentage as the value to the dictionary occupdict
-                line_count += 1                                 #increase line count
-
+                occupdict[row[0]] = float(row[1])           #add the occupation as the key and the percentage as the value to the dictionary occupdict
+            line_count += 1
     return occupdict
     ##print(occupdict)
 
@@ -22,17 +21,19 @@ def genArray(occupdict):
 
     for key in occupdict.keys():
         for i in range(int(occupdict[key]*10)):
-            occuparray.append(occupdict[key])
+            occuparray.append(key)
     return occuparray
-##print(len(occuparray))
+#print(len(occuparray))
 
 ##CHOOSE INDEX OF ARRAY RANDOMLY
 def chooseOccupation(occuparray):
-    return occuparray[random.randint(len(occuparray))]
+    return occuparray[random.randint(0,len(occuparray))]
 
 def main():
     occupdict = genDict('occupations.csv')
+    #print(occupdict)
     occuparray = genArray(occupdict)
+    #print(occuparray)
     occ = chooseOccupation(occuparray)
     print(occ)
     return occ
