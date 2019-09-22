88956b3f8dbaeddb8b9d0a21c3b5865a7d876b8c
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-09-22 18:02:57 -0400

    added test dict and render template code, got both key and value to print

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 import randomOccupation as randOcc
+from collections import OrderedDict
+import jinja2
 
 app = Flask(__name__)
 
+test_dict = {
+    'Gardner': 20,
+    'Firefighter':10
+}
+
 @app.route("/occupyflasks")
 def index():
-    return randOcc.main()
+    return render_template(
+    "template.html",
+    title="Team Chuck Schumer",
+    header="Lauren and Ahmed, pd. 9",
+    tblHeading1="Occupation",
+    tblHeading2="Percent",
+    dict=test_dict
+    )
 
 if __name__ == "__main__":
     app.debug = True
