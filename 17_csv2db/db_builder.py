53610be8a020c3b918e6873848374ac6402557ac
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-10-09 21:12:32 -0400

    finished assignment

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,7 +1,7 @@
 #Lauren Pehlivanian
-#SoftDev
-#skeleton :: SQLITE3 BASICS
-#Oct 2019
+#SoftDev1 pd9
+#17 -- No Trouble
+#2019-10-09
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -16,20 +16,50 @@ c = db.cursor()               #facilitate db ops
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
 
-with open('students.csv') as stucsv:
-     studentsDict = csv.DictReader(stucsv)
-     for row in studentsDict:
-         print(row)
-print("---------------------")
+#REMOVE ROSTER (from when it was created before)
+c.execute("DROP TABLE roster")
+
+#create table with fields from both csv files
+# only create table if already doesn't exist (prevents duplicate error)
+command = "CREATE TABLE IF NOT EXISTS roster (id INTEGER, name TEXT, age INTEGER, code TEXT, mark INTEGER)"          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+# print("---------------------")
+
 with open('courses.csv') as courcsv:
     coursesDict = csv.DictReader(courcsv)
     for row in coursesDict:
-        print(row)
+        # insert statement, fills in fields in this dict, othwersie null.
+        cmd = "INSERT INTO roster VALUES("+row['id']+", NULL, NULL, '"+row['code']+"', "+row['mark']+")"
+        c.execute(cmd)
+        #print(row)
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+with open('students.csv') as stucsv:
+     studentsDict = csv.DictReader(stucsv)
+     for row in studentsDict:
+         #print(row)
+         # update age and name info for all rows(each row- based on id)
+         cmd = "UPDATE roster SET name='"+row['name']+"', age="+row['age']+" WHERE id="+row['id']
+         print(cmd)
+         c.execute(cmd)
+
+#create table with fields from both csv files
+# command = "CREATE TABLE IF NOT EXISTS roster (id INTEGER, name TEXT, age INTEGER, code TEXT, mark INTEGER)"          # test SQL stmt in sqlite3 shell, save as string
+# c.execute(command)    # run SQL statement
+
+#populate roster with values from students dict acc to field
+# for row in studentsDict:
+#     c.execute("INSERT INTO roster INT NULL")
+#     for key in row:
+#         cmd = "INSERT "
+
+#print roster
 
 #==========================================================
 
 db.commit() #save changes
+
+c.execute("SELECT * FROM roster")
+#print(c.fetchall())
+
 db.close()  #close database
