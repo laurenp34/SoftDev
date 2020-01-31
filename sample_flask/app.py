775ab8b574abadfe020f166b6b2665ee87694fe4
Lauren P <laurenpehlivanian@gmail.com>
2020-01-31 03:00:14 +0000

    added app code

--- sample_flask/app.py
+++ sample_flask/app.py
@@ -3,8 +3,6 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-
 
 @app.route('/')
 def hello():
@@ -16,4 +14,4 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='64.227.86.13')
