f4abb9bb8559a3c2c1b1a447f65da3e1bce386a8
Lauren <lpehlivanian00@stuy.edu>
2020-02-12 22:12:07 -0500

    finished hw

--- 07_canvas-anim/dotconn.js
+++ 07_canvas-anim/dotconn.js
@@ -1,34 +1,46 @@
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    prevX = -1;
-    prevY = -1; //reset prevX,Y
+function animaniate(e) {
+    console.log(e);
+		//e.preventDefault();
+    //only create new animation frame if animation stopped
+    //otherwise, animation would keep speeding up if already animating.
+    if (!animate) {
+		    animate = true;
+        id = window.requestAnimationFrame(paintSlide);
+    }
 }
 
-function animaniate(e) {
-		//e.preventDefault();
-		animate = true;
-				
+function stop() {
+  if (animate) {
+    animate = false;
+    window.cancelAnimationFrame(id);
+  }
 }
 
 function paintSlide(timestamp) {
 	if (animate) {
+    ctx.clearRect(0,0,600,600);
 		ctx.beginPath(); //begins a path (to draw arc for circle) based on where the user's mouse is
 		//draw circumference of circle
 		ctx.arc(300,300, radius, 0, 2 * Math.PI);
+    console.log("drew circle radius "+radius);
 		ctx.stroke();
 		ctx.fill(); //fill in circle shape
-		if (expand) radius += 5;
-		else radius -= .1;
-		if (radius >= 300) expand = false;
-		if (radius <= .5) expand = true;
+		if (expand) radius += 2;
+		else radius -= 2;
+		if (radius >= 300) {
+      ctx.strokeStyle = "#FF0000";
+      expand = false;
+    }
+		if (radius <= .5) {
+      ctx.fillStyle = "#5799EC";
+      expand = true;
+    }
 	}
-		
-		window.requestAnimationFrame(paintSlide);		
+
+		id = window.requestAnimationFrame(paintSlide);
 }
 
 var canvas, ctx, window, id;
-var prevX= -1; //not in canvas yet (no prev)
-var prevY= -1;
 var animate = false;
 var radius = 0.5;
 var expand = true;
@@ -36,7 +48,8 @@ var expand = true;
 function setup() {
     canvas = document.getElementById('playground');
     ctx = canvas.getContext("2d");
-    
+    ctx.fillStyle = "#5799EC";
+
     window = document.defaultView;
     id = window.requestAnimationFrame(paintSlide);
 }
