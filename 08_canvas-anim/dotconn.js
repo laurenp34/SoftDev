64afcc846df55fb99f41c2d07ea667404ee974e0
Lauren <lpehlivanian00@stuy.edu>
2020-02-14 00:32:35 -0500

    pingu

--- 08_canvas-anim/dotconn.js
+++ 08_canvas-anim/dotconn.js
@@ -12,11 +12,11 @@ function stop() {
 
 function dvd() {
 	if( mode != "dvd"){
-		mode = "dvd"
-		logo_x = 15;
-		logo_y = 15;
-		logo_vx = 6;
-		logo_vy = 4;
+	    mode = "dvd"
+	    logo_x = Math.random()*400;
+	    logo_y = Math.random()*400;
+	    logo_vx = Math.random() * 14 - 7;
+	    logo_vy = Math.random() * 14 - 7;
 	}
 }
 
@@ -43,13 +43,14 @@ function paintSlide(timestamp) {
 		ctx.clearRect(0,0,600,600);
 		logo_x += logo_vx;
 		logo_y += logo_vy;
-		if(logo_x <= 0 || logo_x >= 600){
+		if(logo_x <= 0 || logo_x >= 600 - logo.naturalWidth*scale){
 			logo_vx *= -1;
 		}
-		if(logo_y <= 0 || logo_y >= 600){
+	    if(logo_y <= 0 || logo_y >= 600 - logo.naturalHeight*scale){
 			logo_vy *= -1;
 		}
-		ctx.drawImage(logo,logo_x,logo_y,20,20);
+	    ctx.drawImage(logo,logo_x,logo_y,logo.naturalWidth*scale,logo.naturalHeight*scale);
+	    
 	}
 
 		id = window.requestAnimationFrame(paintSlide);
@@ -59,6 +60,7 @@ var canvas, ctx, window, id;
 var mode = "stop";
 var radius = 0.5;
 var expand = true;
+var scale = 0.25;
 var logo, logo_x, logo_y,logo_vx,logo_vy; // the same vibe as netlogo, the x and y coordinates + the distance they should move in each direction with each new 'tick'
 
 function setup() {
@@ -66,10 +68,11 @@ function setup() {
     ctx = canvas.getContext("2d");
     ctx.fillStyle = "#5799EC";
 
-    window = document.defaultView;
-    id = window.requestAnimationFrame(paintSlide);
-    
     logo = new Image();
     logo.src = "pingu.jpg"
+
+    window = document.defaultView;
+    id = window.requestAnimationFrame(paintSlide);
+
 }
 
