16dc7e441aff06e1d57550bda77661f72a47d2b4
Lauren <lpehlivanian00@stuy.edu>
2020-02-28 00:02:24 -0500

    m

--- 09_mongo/file.py
+++ 09_mongo/file.py
@@ -3,13 +3,19 @@ client = MongoClient(port=27017)
 import json
 from bson.json_util import loads
 
-db = client.restaurants # creates if doesn't already exist
+#create test database
+db = client['test'] # creates if doesn't already exist
+#create restaurants collection
+restaurants = db['restaurants']
 
 def get_data():
     json_file = open('data2.json')
     stuff = json_file.readlines()
     for line in stuff:
         restaurants.insert_one(loads(line))
+
+get_data()
+len = restaurants.count()
 # json_string = json_file.read()
 # json_string.replace("$date", "date")
    # data = json.load(json_file)
