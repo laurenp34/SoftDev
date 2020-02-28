848223583e5bfd153a668624a59ea2d5f91c719b
Lauren <lpehlivanian00@stuy.edu>
2020-02-27 23:36:38 -0500

    reading from file and creating dictionary works

--- 09_mongo/file.py
+++ 09_mongo/file.py
@@ -1,10 +1,15 @@
 from pymongo import MongoClient
-client = MongoClient()
+client = MongoClient(port=27017)
 import json
+from bson.json_util import loads
 
-json_file = open('data.json')
-json_string = json_file.read()
-json_string.replace("$date", "date")
+db = client.restaurants # creates if doesn't already exist
+
+def get_data():
+    json_file = open('data2.json')
+    stuff = json_file.readlines()
+    for line in stuff:
+        restaurants.insert_one(loads(line))
+# json_string = json_file.read()
+# json_string.replace("$date", "date")
    # data = json.load(json_file)
-
-print(len(json_string))
