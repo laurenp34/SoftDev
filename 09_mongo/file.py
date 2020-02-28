04ae692fc8141de4611e18d36f2a7e5ff7289d72
Lauren <lpehlivanian00@stuy.edu>
2020-02-28 00:38:33 -0500

    finally got dict

--- 09_mongo/file.py
+++ 09_mongo/file.py
@@ -1,6 +1,6 @@
+import pymongo, json
 from pymongo import MongoClient
-client = MongoClient(port=27017)
-import json
+client = MongoClient()
 from bson.json_util import loads
 
 #create test database
@@ -9,20 +9,22 @@ db = client['test'] # creates if doesn't already exist
 restaurants = db['restaurants']
 
 def get_data():
-    json_file = open('data2.json')
+    json_file = open('data3.json')
     stuff = json_file.readlines()
     for line in stuff:
         line.replace("$date","date")
         restaurants.insert_one(loads(line,strict=False))
 
-def ingest(f):
-    with open(f) as _f:
-        return loads(f'[{",".join(map(lambda s: s[:-1], _f))}]')
+# def ingest(f):
+#     with open(f) as _f:
+#         return loads(f'[{",".join(map(lambda s: s[:-1], _f))}]')
 
-#get_data()
-data = ingest("data.json")
-restaurants.insert(data)
-len = restaurants.count()
+if restaurants.estimated_document_count() == 0:
+    get_data()
+#data = ingest("data.json")
+#restaurants.insert(data)
+len = restaurants.estimated_document_count()
+print(len)
 # json_string = json_file.read()
 # json_string.replace("$date", "date")
    # data = json.load(json_file)
